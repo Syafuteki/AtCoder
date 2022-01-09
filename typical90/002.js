84788648ae15f13a7da20285adf5d26f5cc01ba6
syafuteki <syafuteki@gmail.com>
2022-01-09 19:55:05 +0900

    AC20

--- typical90/002.js
+++ typical90/002.js
@@ -9,15 +9,37 @@ function Main(input) {
     //bit全探索（種類が2個なので2進数）
     let i = 0;
     while (i < 2 ** N) {
-        //この10進数を2進数になおしてカウント
-        let count = String(i.toString(2));
-        let zero = count.match(/0/g).length;
-        let one = count.match(/1/g).length;
-        if (zero == one || もういっこの条件入れる) {
-
-        }
+        //10進数を2進数になおす　bit全探索
+        let count = String(i.toString(2)).padStart(N, '0');
+        //配列に入れなおす　書き方の問題で
+        let array = count.split('');
+        //左から数えて左カッコのほうが右カッコより多い　条件1
+        let left = 0;
+        let right = 0;
+        let flag = array.some(element => {
+            if (element == 0) {
+                left++;
+            } else {
+                right++;
+            }
+            if (left < right) return true;
+        });
+        if (left == right && flag == false) console.log(Brackets(array));
+        i++
     }
 
+}
+
+function Brackets(array) {
+    let brackets = '';
+    array.forEach(element => {
+        if (element == 0) {
+            brackets = brackets + '(';
+        } else {
+            brackets = brackets + ')';
+        }
+    });
+    return brackets;
 }
 
 Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
