8f62f80d48fed6a07b9ad951ba2d3b7b40fd7910
syafuteki <syafuteki@gmail.com>
2022-02-09 03:06:59 +0900

    AC5/WA20/TLE1

--- typical90/003.js
+++ typical90/003.js
@@ -1,59 +1,41 @@
+"use strict";
+
 function Main(input) {
     //入力を整理
     input = input.trim().split(/ |\n/).map(Number);
     const N = input.shift();
-
-    let max_bfs = bfs(input, N);
-    let max_dfs = dfs(input, max_bfs, N);
-
-    console.log(max_dfs);
+    let array = bfs(input, input[0], N);
+    array = bfs(input, array[0].indexOf(Math.max(...array[0])), N);
+    console.log(array[1].length);
 }
 
-function bfs(input, N) {
-    //ループ回数
-    let count = 0;
+function bfs(input, start, N) {
     //BFSで最短距離の中の最大値を求める
-    //次行ける候補地　初期値は配列の最初にする
-    let queue = [input[0]];
-    //初期値からの距離
-    let bfs_distance = new Array(N).fill(0);
-    while (count < queue.length) {
-        //現在地
-        const current = queue[count];
-        input.forEach((element, index) => {
-            if (index % 2 != 0) return;
-            if (current == element && queue.includes(input[index + 1]) == false) {
-                queue.push(input[index + 1]);
-                bfs_distance[input[index + 1]] = bfs_distance[current] + 1;
-            }
-        });
-        count++;
-    }
-
-    return bfs_distance.indexOf(Math.max(...bfs_distance));
-}
-
-function dfs(input, max_bfs, N) {
-    //次行ける候補地　初期値はbfsで調べた値
-    let stack = [max_bfs];
-    //1度探索したかどうか
+    //次行ける候補地
+    let queue = [start];
+    //startからの距離の配列
+    let distance = new Array(N).fill(-1);
     let seen = new Array(N).fill(false);
     let list = [];
-    while (stack.length > 0) {
-        const current = stack.pop();
+    while (queue.length > 0) {
+        //現在地
+        const current = queue.shift();
         list.push(current);
         input.forEach((element, index) => {
             if (index % 2 != 0 || seen[current]) return;
             if (current == element && list.includes(input[index + 1]) == false) {
-                stack.push(input[index + 1]);
+                queue.push(input[index + 1]);
+                distance[input[index + 1]] = distance[current] + 1;
                 seen[current] = true;
             } else if (current == input[index + 1] && list.includes(element) == false) {
-                stack.push(element);
+                queue.push(element);
+                distance[current] = distance[input[index + 1]] + 1;
                 seen[current] = true;
             }
         });
     }
-    return list.length;
+
+    return [distance, list];
 }
 
 Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
