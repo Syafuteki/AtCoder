f906d1f5d04369deda5495b2b5ac214980d81e8c
syafuteki <syafuteki@gmail.com>
2022-02-10 01:17:38 +0900

    AC26

--- typical90/003.js
+++ typical90/003.js
@@ -4,38 +4,43 @@ function Main(input) {
     //入力を整理
     input = input.trim().split(/ |\n/).map(Number);
     const N = input.shift();
-    let array = bfs(input, input[0], N);
-    array = bfs(input, array[0].indexOf(Math.max(...array[0])), N);
-    console.log(array[1].length);
+
+    //隣接リストを作成
+    const G = new Array(N).fill(null).map(() => []);
+    input.forEach((element, index) => {
+        if (index % 2) return;
+        G[input[index] - 1].push(input[index + 1] - 1);
+        G[input[index + 1] - 1].push(input[index] - 1);
+    });
+
+    //1回目
+    let distance = bfs(G, 0, N);
+    let max_dfs = distance.indexOf(Math.max(...distance));
+    //2回目
+    distance = bfs(G, max_dfs, N);
+    max_dfs = Math.max(...distance);
+    //最後は閉路にするため、プラス1
+    console.log(max_dfs + 1);
 }
 
-function bfs(input, start, N) {
-    //BFSで最短距離の中の最大値を求める
+//BFSで最短距離の中の最大値を求める
+function bfs(G, start, N) {
     //次行ける候補地
-    let queue = [start];
+    const queue = [start];
     //startからの距離の配列
-    let distance = new Array(N).fill(-1);
-    let seen = new Array(N).fill(false);
-    let list = [];
+    const distance = new Array(N).fill(-1);
+    distance[start] = 0;
     while (queue.length > 0) {
         //現在地
-        const current = queue.shift();
-        list.push(current);
-        input.forEach((element, index) => {
-            if (index % 2 != 0 || seen[current]) return;
-            if (current == element && list.includes(input[index + 1]) == false) {
-                queue.push(input[index + 1]);
-                distance[input[index + 1]] = distance[current] + 1;
-                seen[current] = true;
-            } else if (current == input[index + 1] && list.includes(element) == false) {
-                queue.push(element);
-                distance[current] = distance[input[index + 1]] + 1;
-                seen[current] = true;
-            }
+        const current = queue.pop();
+        G[current].forEach(element => {
+            if (distance[element] != -1) return;
+            distance[element] = distance[current] + 1;
+            queue.push(element);
         });
     }
 
-    return [distance, list];
+    return distance;
 }
 
 Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
