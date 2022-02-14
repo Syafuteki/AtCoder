47007fe03c7a9c02210a5466f4133cc1423257ba
syafuteki <syafuteki@gmail.com>
2022-02-14 19:29:06 +0900

    AC15

--- typical90/007.js
+++ typical90/007.js
@@ -2,27 +2,41 @@
 
 function Main(input) {
     //入力を整理
-    input = input.trim().split(/ |\n/).map(String);
-    const N = Number(input[0]);
-    const K = Number(input[1]);
-    const S = input[2];
+    input = input.trim().split(/\n/).map(String);
+    const N = Number(input.shift());
+    const A = input.shift().split(/ /).map(Number).sort((a, b) => a - b);
+    const Q = Number(input.shift());
+    const B = input.map(Number);
 
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let T = "";
-    let start = 0;
-    for (let i = 0; i < K; i++) {
-        for (let j = 0; j < alphabet.length; j++) {
-            //Tに追加した文字より左はみない
-            const num = S.indexOf(alphabet[j], start);
-            //辞書順でK文字確保しないといけないのであらかじめ後ろの文字を削って調べると速い
-            if (num != -1 && num <= N - K + i) {
-                T += alphabet[j];
-                start = num + 1;
-                break;
-            }
+    //差が1番小さいのを求める
+    B.forEach(e => {
+        const array = binarySearch(A, e, N);
+        const diff_1 = Math.abs(array[0] - e);
+        const diff_2 = Math.abs(array[1] - e);
+        console.log(Math.min(diff_1, diff_2));
+    });
+}
+
+//2分探索
+function binarySearch(A, element, N) {
+    let min = 0;
+    let max = N - 1;
+    while (min <= max) {
+        let mid = Math.floor((min + max) / 2);
+        if (A[mid] == element) {
+            //配列内にBの要素（element）がある時は、ここでreturnする
+            return [A[mid], A[mid]];
+        } else if (A[mid] < element) {
+            min = mid + 1;
+        } else {
+            max = mid - 1;
         }
     }
-    console.log(T);
+    //0と-1比較の時は-1が配列参照できないため、ここでreturnする
+    if (max + min < 0) return [A[0], A[0]];
+    //NとN-1比較の時はNが配列参照できないため、ここでreturnする
+    if (max + min > (N - 1) * 2) return [A[N - 1], A[N - 1]];
+    return [A[min], A[max]];
 }
 
 Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
