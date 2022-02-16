4ea3655d2439eddc3d2470f9e1e88b5c5100e799
syafuteki <syafuteki@gmail.com>
2022-02-16 22:43:54 +0900

    init

---
+++ typical90/008.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function Main(input) {
+    //入力を整理
+    input = input.trim().split(/\n/).map(String);
+    const N = Number(input.shift());
+    const S = input.shift();
+    const num = 10 ** 9 + 7;
+
+}
+
+Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
