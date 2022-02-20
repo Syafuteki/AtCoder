c0283fe1d5d7512f65f391de42a1559affb7bf0a
syafuteki <syafuteki@gmail.com>
2022-02-20 23:37:59 +0900

    AC21

--- typical90/008.js
+++ typical90/008.js
@@ -5,8 +5,20 @@ function Main(input) {
     input = input.trim().split(/\n/).map(String);
     const N = Number(input.shift());
     const S = input.shift();
-    const num = 10 ** 9 + 7;
+    const mod = 10 ** 9 + 7;
 
+    const hash = { a: 1, t: 2, c: 3, o: 4, d: 5, e: 6, r: 7 };
+    //dp[-1]を防ぐために、1つ増やす
+    const dp = Array(8).fill(0);
+    //dp[0]はaの一通り分、入れるため1
+    dp[0] = 1;
+
+    //DP法
+    for (const s of S) {
+        dp[hash[s]] = (dp[hash[s]] + dp[hash[s] - 1]) % mod;
+    }
+
+    console.log(dp[7]);
 }
 
 Main(require('fs').readFileSync('/dev/stdin', 'utf8'));
